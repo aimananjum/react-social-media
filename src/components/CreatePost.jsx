2f91cd699cf2479c05c6d7ed5c0aafd6f1df25a4
aimananjum <aimananjum2001@gmail.com>
2023-12-07 15:55:28 +0530

    social media version 1

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,116 @@
+import { useContext } from "react";
+import { useRef } from "react";
+import { PostListContext } from "../store/post-list-store";
+
+const CreatePost = () => {
+  const userId = useRef();
+  const postTitle = useRef();
+  const postBody = useRef();
+  const reactions = useRef();
+  const tags = useRef();
+  const { addPost } = useContext(PostListContext);
+
+  const handleSubmit = (
+    event,
+    userId,
+    postTitle,
+    postBody,
+    reactions,
+    tags
+  ) => {
+    event.preventDefault();
+    let tagsArray = tags.current.value.split(" ");
+
+    let post = {
+      id: Date.now(),
+      title: postTitle.current.value,
+      body: postBody.current.value,
+      reaction: reactions.current.value,
+      userId: userId.current.value,
+      tags: tagsArray,
+    };
+    postTitle.current.value = "";
+    postBody.current.value = "";
+    reactions.current.value = "";
+    userId.current.value = "";
+    tags.current.value = "";
+    addPost(post);
+  };
+  return (
+    <form
+      className="create-post"
+      onSubmit={(event) =>
+        handleSubmit(event, userId, postTitle, postBody, reactions, tags)
+      }
+    >
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          User Id
+        </label>
+        <input
+          ref={userId}
+          type="text"
+          className="form-control"
+          id="userid"
+          placeholder="Enter your user-id"
+        />
+      </div>
+      <div className="mb-3 ">
+        <label htmlFor="exampleInputEmail1" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitle}
+          className="form-control"
+          id="title"
+          placeholder="How you are feeling today..."
+          aria-describedby="emailHelp"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          ref={postBody}
+          rows="4"
+          className="form-control"
+          id="content"
+          placeholder="Tell us more about this"
+        />
+      </div>
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Number of Reactions
+        </label>
+        <input
+          type="number"
+          ref={reactions}
+          className="form-control"
+          id="reactions"
+          placeholder="how many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="exampleInputPassword1" className="form-label">
+          Enter your Hashtags
+        </label>
+        <input
+          type="text"
+          ref={tags}
+          className="form-control"
+          placeholder="Enter the hashtags (space seperated)"
+          id="hastags"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+};
+export default CreatePost;
---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,66 @@
+import { createContext, useReducer } from "react";
+
+export const PostListContext = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const PostListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  } else if (action.type === "ADD_POST") {
+    newPostList = [action.payload.post, ...currPostList];
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    PostListReducer,
+    DEFAULT_POST_LIST
+  );
+  const addPost = (post) => {
+    dispatchPostList({
+      type: "ADD_POST",
+      payload: { post },
+    });
+  };
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostListContext.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostListContext.Provider>
+  );
+};
+export default PostListProvider;
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to mumbai",
+    body: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    reaction: 2,
+    userId: "user-3",
+    tags: ["Hello world", "How are you"],
+  },
+  {
+    id: "2",
+    title: "Going to mudfnvdfnvdnf mbai",
+    body: "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry",
+    reaction: 15,
+    userId: "user-9",
+    tags: ["Hello world", "How are you"],
+  },
+];
